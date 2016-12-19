6e8316b413ba66d29e77a90e46147153d5fa4e40
nvertigo <nvertigo67@gmail.com>
2016-12-19 18:56:35 +0100

    audio: revert back to cm-13.0
    Change-Id: Ibc6c11d308dcb41629733d3f260fb493ec5ab817

--- hal/audio/Android.mk
+++ hal/audio/Android.mk
@@ -12,25 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifneq ($(TARGET_DEVICE),ha3g)
+
 LOCAL_PATH := $(call my-dir)
 
 # Audio HAL
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := audio_hw.c ril_interface.c
 
 LOCAL_C_INCLUDES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
 	external/tinyalsa/include \
 	$(call include-path-for, audio-effects) \
 	$(call include-path-for, audio-utils) \
 	$(call include-path-for, audio-route)
 
+LOCAL_ADDITIONAL_DEPENDENCIES += \
+	$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+
 LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl \
 	libaudience_voicefx libaudioroute
+
+LOCAL_CFLAGS := -Wno-unused-parameter
 
 include $(BUILD_SHARED_LIBRARY)
 
@@ -41,11 +49,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libaudience_voicefx
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES:= eS325VoiceProcessing.cpp
+LOCAL_SRC_FILES := eS325VoiceProcessing.cpp
 
 LOCAL_C_INCLUDES += \
 	$(call include-path-for, audio-effects)
 
 LOCAL_SHARED_LIBRARIES := liblog libutils
 
+LOCAL_CFLAGS := -Wno-unused-parameter
+
 include $(BUILD_SHARED_LIBRARY)
+
+endif
