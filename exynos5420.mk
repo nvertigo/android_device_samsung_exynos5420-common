a49ce1904ea6b3981d3bece846c8c3ceae13fb2e
nvertigo <nvertigo67@gmail.com>
2016-12-20 10:07:17 +0100

    audio: decommonize audio_policy.conf.
    Change-Id: I8ddd0355b5842548fb25f8b8960fe20678601e86

--- exynos5420.mk
+++ exynos5420.mk
@@ -35,8 +35,7 @@ PRODUCT_PACKAGES += \
     tinymix
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf \
-    $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf
+    $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/etc/audio_effects.conf
 
 # Boot animation
 TARGET_BOOTANIMATION_HALF_RES := true
